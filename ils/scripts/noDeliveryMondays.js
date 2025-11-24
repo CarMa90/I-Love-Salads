c44dbc84682356887cd17dfee314f6d5cd513be5
CarMa90 <el.matteotti@gmail.com>
2025-11-24 16:38:54 -0700

    add padding to header__content and comenting the console.logs

--- ils/scripts/noDeliveryMondays.js
+++ ils/scripts/noDeliveryMondays.js
@@ -1,13 +1,13 @@
 export default function noDelivery() {
   let d = document,
     diaSemana = new Date().getDay();
-  console.log(diaSemana);
+  // console.log(diaSemana);
 
   let $header = d.querySelector(".header"),
     $forjadores = d.querySelector("#forjadores .card__title");
 
   if (diaSemana == 1) {
-    console.log("hola desde un lunes");
+    // console.log("hola desde un lunes");
     let $p = d.createElement("p");
     $p.classList.add("header__message");
     $p.innerHTML =
